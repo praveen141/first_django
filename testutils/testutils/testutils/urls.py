5adba1bc8fea829599b5927118ee027af5cdcbce
praveen <praveen.gupta141@gmail.com>
2021-07-06 16:34:24 +0530

    code completed

--- testutils/testutils/testutils/urls.py
+++ testutils/testutils/testutils/urls.py
@@ -19,10 +19,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    # path('about', views.about, name='about'),
-    path('analyze', views.analyze, name='analyze'),
-    # path('capitalizefirst', views.capfirst, name='capfirst'),
-    # path('newlineremove', views.newlineremove, name='newlineremove'),
-    # path('spaceremove', views.spaceremove, name='spaceremove'),
-    # path('charcount', views.charcount, name='charcount'),
+    path('about', views.about, name='about'),
+    path('analyze', views.analyze, name='analyze')
 ]
--- testutils/testutils/testutils/views.py
+++ testutils/testutils/testutils/views.py
@@ -16,65 +16,53 @@ def about(request):
 
 def analyze(request):
     # GET the text
-    djtext = request.GET.get('content', 'default')
+    djtext = request.POST.get('content', 'default')
     # Check checkbox values
-    removepun = request.GET.get('removepunc', 'off')
-    fullcap = request.GET.get('fullcap', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    spaceremover = request.GET.get('spaceremover', 'off')
-    charcount = request.GET.get('charcount', 'off')
+    removepun = request.POST.get('removepunc', 'off')
+    fullcap = request.POST.get('fullcap', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    spaceremover = request.POST.get('spaceremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
 
-    print(removepun)
-    print(djtext)
+    # print(removepun)
+    # print(djtext)
     # GET the text
-    if removepun != 'off':
+    if removepun == 'on':
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
         for i in djtext:
             if i not in punctuations:
-                analyzed=analyzed + i
+                analyzed = analyzed + i
         params = {'purpose': 'remove stops', 'analyzed_text': analyzed}
-        return render(request,'analyze.html',params)
-    elif fullcap != 'off':
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if fullcap == 'on':
         analyzed = djtext.upper()
         params = {'purpose': 'Capitalize Letters', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif newlineremover != 'off':
-        analyzed = djtext.replace('\n', '')
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if newlineremover == 'on':
+        analyzed = ""
+        for char in djtext:
+            if char != '\n'  and char !='\r':
+                analyzed= analyzed +char
+        else:
+            print('no')
+        print("pre" , analyzed)
         params = {'purpose': 'New Line Removers', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif spaceremover != 'off':
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if spaceremover == 'on':
         analyzed = djtext.replace('  ', '')
         params = {'purpose': 'Space Removers', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif charcount != 'off':
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if charcount == 'on':
         count = 0
         for char in djtext:
             count += 1
         analyzed = count
         params = {'purpose': 'Count Character', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
-    #return HttpResponse('''<h2> removepunc </h2>
-    # <a href="http://127.0.0.1:8000/"> removepunc  </a>''')
-
-#
-# def capfirst(request):
-#     return HttpResponse('''<h2> capfirst </h2>
-#     <a href="http://127.0.0.1:8000/"> capfirst  </a>''')
-#
-#
-# def newlineremove(request):
-#     return HttpResponse('''<h2> newlineremover </h2>
-#     <a href="http://127.0.0.1:8000/"> newlineremover  </a>''')
-#
-#
-# def charcount(request):
-#     return HttpResponse('''<h2> charcount </h2>
-#     <a href="https://en.wikipedia.org/wiki/Chandigarh"> charcount  </a>''')
-#
-#
-# def spaceremove(request):
-#     return HttpResponse('''<h2> spaceremove </h2>
-#     <a href="https://en.wikipedia.org/wiki/Chandigarh"> spaceremove  </a>''')
+    if (removepun != 'on' and fullcap != 'on' and newlineremover != 'on' and spaceremover != 'on' and charcount != 'on'):
+        return HttpResponse ('Please select any operation and try again')
+    return render(request, 'analyze.html', params)
